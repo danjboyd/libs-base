b07e477aba4fe266781ee6edf752053682a17cd3
Daniel Boyd <dboyd@invitoep.com>
2026-04-17 15:15:15 -0500

    Tighten NSData and NSURL Swift nullability

--- Headers/Foundation/NSURL.h
+++ Headers/Foundation/NSURL.h
@@ -80,7 +80,12 @@ GS_EXPORT_CLASS
   void		*_Nullable _data;
 #endif
 }
- 
+
+/**
+ * Initialize an empty URL object.
+ */
+- (nonnull instancetype) init;
+
 /**
  * Create and return a file URL with the supplied path.<br />
  * The value of aPath must be a valid filesystem path.<br />
@@ -783,5 +788,4 @@ GS_NSURLComponents_IVARS;
 #endif	/* GS_API_MACOSX */
 
 #endif	/* __NSURL_h_GNUSTEP_BASE_INCLUDE */
-
 
--- Source/NSURL.m
+++ Source/NSURL.m
@@ -700,6 +700,11 @@ static NSUInteger	urlAlign;
   return nil;
 }
 
+- (id) init
+{
+  return [self initWithString: @""];
+}
+
 - (id) initFileURLWithPath: (NSString *)aPath
 {
   /* isDirectory flag will be overwritten if a directory exists. */
